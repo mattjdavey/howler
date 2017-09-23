35563f7263bf8837ee911a10fe354643edd8d6f0
Matt Davey <matt.davey12@gmail.com>
2017-09-23 17:10:07 -0500

    Connected app to firebase database.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,44 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { SettingsPage } from '../pages/settings/settings'; 
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCr8W3twk5NhrL4ooLUrlHAJqZkaBdrMjo",
+  authDomain: "howler-f0fd3.firebaseapp.com",
+  databaseURL: "https://howler-f0fd3.firebaseio.com",
+  projectId: "howler-f0fd3",
+  storageBucket: "howler-f0fd3.appspot.com",
+  messagingSenderId: "208442285435"
+};
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    SettingsPage
+    
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    SettingsPage
+    
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  songs: FirebaseListObservable<any>;
+  
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public angularFire: AngularFireDatabase, public alertCtrl: AlertController) {
+    this.songs = angularFire.list('/songs');
   }
 
+  addSong(){
+    let prompt = this.alertCtrl.create({
+      title: 'Song Name',
+      message: "Enter a name for this new song you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.songs.push({
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+ 
+
 }
+
+
